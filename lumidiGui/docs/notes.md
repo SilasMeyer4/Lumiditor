73406fe2022c11bebbff586a806a7325d72a1a36
SilasMeyer4 <meyer-si4@web.de>
2025-07-18 16:53:08 +0200

    added notes for architector

---
+++ lumidiGui/docs/notes.md
@@ -0,0 +1,19 @@
+# Notes 2025-07-18
+
+## UIElementBase 
+
+- Vector3 position 
+- Vector3 size
+- so that i can use UIElement3D later
+
+
+## Fluent API 
+
+- Adding FluentAPI that handles all the .lock() and logs error for that
+
+## Collider 
+
+- Using a collider map in UIElement, Collider Keys. std::unordnneredmap, shared ptr.
+- Behaviors get Collider keys, that they use to access the collider information
+- UIColliderBase -> UICollider2D, UICollider3D
+  
---
+++ lumidiGui/include/fluentAPI/fluentHandlerUIElements.h
@@ -0,0 +1,41 @@
+#ifndef LUMIDIGUI_FLUENTAPI_FLUENT_HANDLER_UIELEMENTS_H
+#define LUMIDIGUI_FLUENTAPI_FLUENT_HANDLER_UIELEMENTS_H
+
+#include "fluentHandler.h"
+#include "concepts.h"
+
+namespace LumidiGui
+{
+  namespace FluentAPI
+  {
+    template <DerivedFromUIElement2D T>
+    class FluentHandlerUIElements : public FluentHandler<T>
+    {
+    private:
+      std::shared_ptr<UIElement2D> LockAsBase() const
+      {
+        return std::static_pointer_cast<UIElement2D>(weakPtr_.lock());
+      }
+
+    public:
+      using FluentHandler<T>::weakPtr_;
+      FluentHandlerUIElements(std::weak_ptr<T> ptr) : FluentHandler<T>(ptr) {}
+
+      template <DerivedFromUIBehavior Behavior, typename... Args>
+      FluentHandlerUIElements<T> &AddBehavior(Args &&...args)
+      {
+        if (auto ptr = LockAsBase())
+        {
+          ptr->AddBehavior<Behavior>(std::forward<Args>(args)...);
+        }
+        else
+        {
+          std::cerr << "[Fluent API] Warning: Element no longer exists." << std::endl;
+        }
+        return *this;
+      }
+    };
+  }
+}
+
+#endif
